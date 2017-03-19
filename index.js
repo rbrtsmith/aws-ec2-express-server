a2c5120d382fc55e91f152e00308817054eaaaa6
Robert Smith <rbrtsmith84@googlemail.com>
2017-03-19 19:02:32 +0000

    Add quotes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
 
 const app = express()
 const port = 8080
+const data = fs.readFileSync(path.join(__dirname, 'quotes.json'), 'utf8')
+
+const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)]
 
 app.get('/', (req, res) => {
-  res.send('Hello AWS')
+  const quotes = JSON.parse(data).quotes
+  const quote = getRandomItem(quotes)
+  res.send(quote)
 })
 
 app.listen(port, console.log(`listening on port ${port}`))
