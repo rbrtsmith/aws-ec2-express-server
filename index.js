838f45757e06c9b5e9c2a3ff5319df1533175030
Robert Smith <rbrtsmith84@googlemail.com>
2017-03-19 19:51:44 +0000

    Add routes and EJS templates

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
+app.set('view engine', 'ejs');
+
+
 const port = 8080
-const data = fs.readFileSync(path.join(__dirname, 'quotes.json'), 'utf8')
+const file = path.join(__dirname, 'quotes.json')
+const data = fs.readFileSync(file, 'utf8')
+const quotes = JSON.parse(data)
 
 const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)]
 
 app.get('/', (req, res) => {
-  const quotes = JSON.parse(data).quotes
-  const quote = getRandomItem(quotes)
-  res.send(quote)
+  res.render('index', { quotes })
+})
+
+app.get('/:quotePath', (req, res) => {
+  const quote = quotes.filter(quote => quote.path === req.params.quotePath)[0]
+  console.log(quote)
+  res.render('quote', { quote })
 })
 
 app.listen(port, console.log(`listening on port ${port}`))
